f935192fd38a7fd94387fcc166cad69463cc2549
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-07-17 02:01:53 +0100

    remove sprockets

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -164,9 +164,9 @@ module ActiveAdmin
     private
 
     def register_default_assets
-      # register_stylesheet 'active_admin.css', media: 'screen'
+      register_stylesheet 'active_admin.css', media: 'screen'
       # register_stylesheet 'active_admin/print.css', media: 'print'
-      # register_javascript 'active_admin.js'
+      register_javascript 'active_admin.js'
     end
 
     # Since app/admin is alphabetically before app/models, we have to remove it
